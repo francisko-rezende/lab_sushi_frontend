e636cb31780891b1222acf101ab8d638a4f9302d
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-03-23 20:38:47 -0300

    termina "[M3S06] Ex 1 - Revisando roteamento"

---
+++ src/pages/Orders/Orders.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Orders = () => {
+  return <h1>Meus pedidos</h1>;
+};
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Cart from '../pages/Cart';
+import { Contact } from '../pages/Contact';
 import Details from '../pages/Details';
 
 import Home from '../pages/Home';
 import Order from '../pages/Order';
+import { Orders } from '../pages/Orders';
 
 const AppRoutes = () => {
   return (
@@ -14,6 +16,8 @@ const AppRoutes = () => {
       <Route path="details/:id" element={<Details />} />
       <Route path="cart" element={<Cart />} />
       <Route path="order" element={<Order />} />
+      <Route path="my-orders" element={<Orders />} />
+      <Route path="contact" element={<Contact />} />
     </Routes>
   );
 }
