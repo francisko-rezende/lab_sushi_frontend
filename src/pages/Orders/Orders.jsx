61e0b53d980477a97dacd8b95e77d573cf0f6c03
Francisko de Moraes Rezende <francisko.rezende@gmail.com>
2023-03-23 20:56:18 -0300

    termina "[M3S06] Ex 2 - Revisando useState e useEffect"

--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,5 +1,28 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
 export const Orders = () => {
-  return <h1>Meus pedidos</h1>;
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("http://localhost:3333/orders")
+      .then((response) => response.json())
+      .then((data) => setData(data));
+  }, []);
+
+  console.log(data);
+
+  return (
+    <>
+      <h1>Meus pedidos</h1>
+      <ul>
+        {data &&
+          data.map((order) => {
+            return (
+              <li key={order.clientName}>
+                Nome do cliente: {order.clientName}
+              </li>
+            );
+          })}
+      </ul>
+    </>
+  );
 };
